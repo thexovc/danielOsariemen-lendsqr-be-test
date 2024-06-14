da2164620ab793bdec72c89056e75ce580a50c92
thexovc <osazeepeter79@gmail.com>
2024-06-14 21:01:04 +0100

    created wallet table

---
+++ migrations/20240614165635_create_wallets_table.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('wallets', function (table) {
+    table.increments('id').primary();
+    table.integer('user_id').unsigned().notNullable();
+    table.double('balance').defaultTo(0);
+    table
+      .enum('currency', ['NGN', 'EUR', 'USD'])
+      .notNullable()
+      .defaultTo('NGN');
+    table.timestamps(true, true);
+
+    table
+      .foreign('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('wallets');
+}
