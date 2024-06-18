0255f778f8e02ff54f141cc6c5abc1de5d5aec4b
thexovc <osazeepeter79@gmail.com>
2024-06-18 16:39:31 +0100

    configured knex provider with node_env

--- src/knex/knex.provider.ts
+++ src/knex/knex.provider.ts
@@ -1,13 +1,25 @@
 import { Provider } from '@nestjs/common';
 import * as Knex from 'knex';
 import KnexConfig from '../../knexfile';
+import { ConfigService } from '@nestjs/config';
 
 export const KNEX_CONNECTION = 'KNEX_CONNECTION';
 
 export const knexProvider: Provider = {
   provide: KNEX_CONNECTION,
-  useFactory: async () => {
-    const knex = Knex(KnexConfig.production);
+  inject: [ConfigService],
+  useFactory: async (configService: ConfigService) => {
+    const nodeEnv = configService.get<string>('NODE_ENV') || 'development';
+
+    let knexConfig: any;
+
+    if (nodeEnv === 'production') {
+      knexConfig = KnexConfig.production;
+    } else {
+      knexConfig = KnexConfig.development;
+    }
+
+    const knex = Knex(knexConfig);
     return knex;
   },
 };
