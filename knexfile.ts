df59fed7c838b8d4aa558a4c344a2613ae8c6983
thexovc <osazeepeter79@gmail.com>
2024-06-18 16:36:44 +0100

    added knex production configuration

--- knexfile.ts
+++ knexfile.ts
@@ -15,26 +15,11 @@ const config: { [key: string]: Knex.Config } = {
     },
   },
 
-  staging: {
-    client: 'mysql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 16237,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
