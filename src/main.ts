3fc049e934b399b0cfa1a9305e18e25409220620
thexovc <osazeepeter79@gmail.com>
2024-06-19 08:50:02 +0100

    fixes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AllExceptionsFilter } from './Exception/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Apply the global exception filter
+  // Apply the global exception filters
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
